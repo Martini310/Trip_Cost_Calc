6972d68cbeb86bd3b58e93e4437df6e7cf1e9cee
Martini310 <martin310@wp.pl>
2022-11-05 23:53:19 +0100

    Reconfigured the result popup window, added popup error windows to empty entries

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from kivy.uix.button import Button
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.popup import PopupException
 from kivy.uix.popup import Popup
+from kivy.core.text.markup import MarkupLabel
 from kivy.uix.gridlayout import GridLayout
 from backend import *
 
@@ -19,6 +20,7 @@ class MyRoot(BoxLayout):
         self.please_wait = None
         self.fuel_price = None
         self.paliwo = 'fuel'
+        self.info = None
 
     def press(self):
         self.please_wait = PleaseWait(title="Please wait", size_hint=(0.8, 0.8), auto_dismiss=False)
@@ -33,47 +35,36 @@ class MyRoot(BoxLayout):
         elif self.fuel_price == 'price':
             self.paliwo = self.ids.price_slider.value
 
-        if start == "" or meta == "":
-            return self.please_wait.dismiss(), Factory.ErrorPopup(content=Label(text="pusto")).open()
+        if start == "":
+            print(self.fuel_price)
+            return self.please_wait.dismiss(), self.layout("Podaj adres początkowy").open()
+        elif meta == "":
+            return self.please_wait.dismiss(), self.layout("Podaj cel podróży").open()
+        elif self.paliwo == "fuel":
+            return self.please_wait.dismiss(), self.layout("Wybierz rodzaj paliwa, lub ustaw cenę").open()
 
         try:
             wynik = TripCost(start, meta, self.paliwo, consumption)
 
-            info = f'Koszt przejazdu z: \n{start} \ndo: \n{meta} \nwynosi {round(wynik.trip_cost, 2)}zł. \n' \
-                   f'Potrwa {wynik.duration} i wyniesie {wynik.distance / 1000}km. \n' \
-                   f'Koszt {self.paliwo if type(self.paliwo) == str else "paliwa"} to {wynik.price}zł/l.'
-            print(info)
+            self.info = f'Koszt przejazdu z: \n{start} \ndo: \n{meta} \nwynosi {round(wynik.trip_cost, 2)}zł. \n' \
+                        f'Potrwa {wynik.duration} i wyniesie {wynik.distance / 1000}km. \n' \
+                        f'Koszt {self.paliwo if type(self.paliwo) == str else "paliwa"} to {wynik.price}zł/l.'
+            print(self.info)
 
-            layout = GridLayout(cols=1, padding=10)
+            test = Factory.Result()
 
-            popup_label = Label(text=info, font_size=24, font_name="Arial")
-            close_button = Button(text="Ok")
+            test.ids['a'].text = start
+            test.ids['b'].text = meta
+            test.ids['c'].text = str(round(wynik.trip_cost, 2))
+            test.ids['d'].text = wynik.duration
+            test.ids['e'].text = str(wynik.distance / 1000)
+            test.ids['f'].text = str(wynik.price)
 
-            layout.add_widget(popup_label)
-            layout.add_widget(close_button)
-
-            # Instantiate the modal popup and display
-            popup = Popup(title='Dane przejazdu',
-                          content=layout,
-                          size_hint=(0.8, 0.8),
-                          auto_dismiss=False)
-            popup.open()
-
-            # Attach close button press with popup.dismiss action
-            close_button.bind(on_press=popup.dismiss)
+            test.open()
             self.please_wait.dismiss()
         except KeyError:
             self.please_wait.dismiss()
-            # Popup layout
-            layout = GridLayout(cols=1, padding=10)
-            popup_label = Label(text="Wybierz rodzaj paliwa lub cenę!", font_size=24, font_name="Arial")
-            close_button = Button(text="Ok")
-            layout.add_widget(popup_label)
-            layout.add_widget(close_button)
-            # Call Popup
-            error = Factory.ErrorPopup(content=layout)
-            error.open()
-            close_button.bind(on_press=error.dismiss)
+            self.layout("Sprawdź, czy adresy,\nktóre podałeś są prawidłowe.\nNie mogę znaleźć takiej trasy").open()
 
     def select_price(self):
         paliwa = self.ids.paliwa
@@ -91,6 +82,16 @@ class MyRoot(BoxLayout):
         price_slider = self.ids.price_slider
         price_slider.height, price_slider.size_hint_y, price_slider.opacity, price_slider.disabled = 0, None, 0, True
 
+    def layout(self, text: str):
+        layout = GridLayout(cols=1, padding=10)
+        popup_label = Label(text=text, font_size=24, font_name="Arial")
+        close_button = Button(text="Ok")
+        layout.add_widget(popup_label)
+        layout.add_widget(close_button)
+        test = Factory.ErrorPopup(content=layout)
+        close_button.bind(on_press=test.dismiss)
+        return test
+
 
 class PleaseWait(Popup):
 
@@ -100,27 +101,6 @@ class PleaseWait(Popup):
 
     def layout(self):
         self.add_widget(Label(text="Proszę czekać.."))
-
-
-# class ErrorPopup(Popup):
-#
-#     error_msg = 'test'
-#
-#     def __init__(self, **kwargs):
-#         super().__init__()
-#         # self.layout()
-#
-#     def layout(self):
-#         pass
-        # layout = GridLayout(cols=1, padding=10)
-        #
-        # popup_label = Label(text='Wybierz rodzaj lub cenę paliwa!', font_size=24, font_name="Arial")
-        # # close_button = Button(text="Ok", on_release=self.dismiss())
-        #
-        # layout.add_widget(popup_label)
-        # # layout.add_widget(close_button)
-
-        # self.add_widget(Label(text='paliwo'))
 
 
 class TripCostCalcApp(App):
